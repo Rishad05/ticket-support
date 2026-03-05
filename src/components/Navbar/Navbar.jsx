8ee56a3149f90dcfcca41f7cbe464a06484caef4
Rishad05 <iu.rishad5@gmail.com>
2026-03-05 10:39:01 +0600

    feat: initialize ticket-support project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created Vite configuration file for React and Tailwind CSS integration.
    - Implemented main application structure with App component and Navbar component.
    - Added global styles with Tailwind CSS and DaisyUI.
    - Included SVG assets for Vite and React logos.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navbar = () => {
+ return (
+  <div className="navbar bg-base-100 shadow-sm w-[90%] mx-auto">
+   <div className="navbar-start">
+    <div className="dropdown">
+     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+     </div>
+     <ul
+      tabIndex="-1"
+      className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+      <li><a>Home</a></li>
+      <li><a> Faq </a></li>
+      <li><a> Changelog </a></li>
+      <li><a> Download </a></li>
+      <li><a> Blog </a></li>
+      <li><a>Contact</a></li>
+     </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">Ticket Support</a>
+   </div>
+   <div className="navbar-end">
+    <ul className="menu menu-horizontal px-2 hidden lg:flex">
+     <li><a>Home</a></li>
+     <li><a> Faq </a></li>
+     <li><a> Changelog </a></li>
+     <li><a> Download </a></li>
+     <li><a> Blog </a></li>
+     <li><a>Contact</a></li>
+    </ul>
+    <a className="bg-linear-to-r from-[#632EE3] to-[#9b5df2] text-white px-3 py-2 rounded-sm font-medium"> + New Ticket</a>
+   </div>
+  </div>
+ );
+};
+
+export default Navbar;
